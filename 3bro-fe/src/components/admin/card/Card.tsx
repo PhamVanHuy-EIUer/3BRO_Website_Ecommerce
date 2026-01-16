709095131c6100028f2ad899b8e0401cac78718b
PhamVanHuy-EIUer <huy.phamvan.cit22@eiu.edu.vn>
2026-01-16 17:41:02 +0700

    update page admin

---
+++ 3bro-fe/src/components/admin/card/Card.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { MdSupervisedUserCircle } from "react-icons/md";
+
+const Card = () => {
+  return (
+    <div>
+      <MdSupervisedUserCircle size={24} />
+    </div>
+  );
+};
+
+export default Card;

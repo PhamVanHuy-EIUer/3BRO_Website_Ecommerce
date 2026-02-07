e9e83c6c4fc8c0d33a166a56fd2b5f28eb5b6a9b
NgHoangHao <haonguyenhoang8@gmail.com>
2026-02-07 13:53:26 +0700

    update

--- Store3BRO/Ecommerce3BRO/Repository/Implement/PaymentRepository.cs
+++ Store3BRO/Ecommerce3BRO/Repository/Implement/PaymentRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Azure.Core;
 using Ecommerce3BRO.Data;
 using Ecommerce3BRO.DTO;
+using Ecommerce3BRO.Helper;
 using Ecommerce3BRO.Helper.Enums;
 using Ecommerce3BRO.Model;
 using Ecommerce3BRO.Service;
@@ -11,9 +12,11 @@ namespace Ecommerce3BRO.Repository.Implement
     public class PaymentRepository : IPaymentRepository
     {
         private readonly Ecommerce3BROContext _context;
-        public PaymentRepository(Ecommerce3BROContext context)
+        private readonly TakeCurrentTime _time;
+        public PaymentRepository(Ecommerce3BROContext context,TakeCurrentTime time)
         {
             _context = context;
+            _time = time;
         }
         public async Task<ApiResponse<GetPaymentDTO>> AddNewPaymentAsync(Guid orderId)
         {
@@ -78,23 +81,23 @@ namespace Ecommerce3BRO.Repository.Implement
 
         }
 
-        public async Task<ApiResponse<GetPaymentDTO>> GetAllPaidPayment()
-        {
-            var payments = await _context.Payment.Include(p => p.Order).ThenInclude(o => o.User).Where(p=>p.Status==1)
-                .OrderByDescending(p => p.CreatedDate)
-               .Select(p => new GetPaymentDTO
-               {
-                   Amount = p.Amount,
-                   CreatedDate = p.CreatedDate,
-                   Id = p.Id,
-                   OrderUserName = p.Order.User.FullName,
-                   PaymentDate = p.PaymentDate,
-                   PaymentMethod = p.PaymentMethod,
-                   Status = ((PaymentStatus)p.Status).ToString(),
-                   TransactionCode = p.TransactionCode
-               }).ToListAsync();
-            return new ApiResponse<GetPaymentDTO>(payments, null, "200", "Get all payment by page successfully", true, 0,0,0,0, null, null, null);
-        }
+        //public async Task<ApiResponse<GetPaymentDTO>> GetAllPaidPayment()
+        //{
+        //    var payments = await _context.Payment.Include(p => p.Order).ThenInclude(o => o.User).Where(p=>p.Status==1)
+        //        .OrderByDescending(p => p.CreatedDate)
+        //       .Select(p => new GetPaymentDTO
+        //       {
+        //           Amount = p.Amount,
+        //           CreatedDate = p.CreatedDate,
+        //           Id = p.Id,
+        //           OrderUserName = p.Order.User.FullName,
+        //           PaymentDate = p.PaymentDate,
+        //           PaymentMethod = p.PaymentMethod,
+        //           Status = ((PaymentStatus)p.Status).ToString(),
+        //           TransactionCode = p.TransactionCode
+        //       }).ToListAsync();
+        //    return new ApiResponse<GetPaymentDTO>(payments, null, "200", "Get all payment by page successfully", true, 0,0,0,0, null, null, null);
+        //}
 
         public async Task<ApiResponse<GetPaymentDTO>> GetAllPaymentByPageAsync(int currentPage, int pageSize)
         {
@@ -117,6 +120,43 @@ namespace Ecommerce3BRO.Repository.Implement
             return new ApiResponse<GetPaymentDTO>(payments, null, "200", "Get all payment by page successfully", true, currentPage, pageSize, totalPages, totalItems, null, null, null);
         }
 
+        public async Task<ApiResponse<TotalSaleDTO>> GetAllSalesByMonth()
+        {
+            var payments = await _context.Payment.Where(p => p.Status == 1).ToListAsync();
+            var currentTime = _time.currentime();
+            var pastTime = _time.sixPastTime();
+            var listSale = payments.Where(p => p.PaymentDate >= pastTime && p.PaymentDate < currentTime)
+                .GroupBy(p => new
+                {
+                    Year = p.PaymentDate.Value.Year,
+                    Month = p.PaymentDate.Value.Month,
+                }).Select(g => new TotalSaleDTO
+                {
+                    Year = g.Key.Year,
+                    Month = ((MonthEnum)g.Key.Month).ToString(),
+                    NumOfProduct = g.Count(),
+                    TotalSale = g.Sum(x => x.Amount)
+                }).ToList();
+            var result = Enumerable.Range(0, 6)
+         .Select(i =>
+    {
+        var date = currentTime.AddMonths(-6 + i); // từ 5 tháng trước → hiện tại
+
+        var sale = listSale.FirstOrDefault(s =>
+            s.Year == date.Year && s.Month == ((MonthEnum)date.Month).ToString());
+        return new TotalSaleDTO
+        {
+            Year = date.Year,
+            Month = ((MonthEnum)date.Month).ToString(),
+            NumOfProduct = sale?.NumOfProduct ??0,
+            TotalSale = sale?.TotalSale ??0
+        };
+    })
+    .ToList();
+
+            return new ApiResponse<TotalSaleDTO>(result, null, "200", "Get all total sales by month successfully", true, 0,0,0,0, null, null, null);
+        }
+
         public async Task<ApiResponse<GetPaymentDTO>> UpdateStatusPayment(Guid paymentId, int status)
         {
             var findPayment = await _context.Payment.FindAsync(paymentId);
@@ -129,5 +169,6 @@ namespace Ecommerce3BRO.Repository.Implement
             return new ApiResponse<GetPaymentDTO>(null, null, "200", "Update payment successfully", true, 0, 0, 0, 0, null, null, null);
 
         }
+
     }
 }

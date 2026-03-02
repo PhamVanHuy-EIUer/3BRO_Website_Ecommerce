c0929ef6a6987288f815d26cc17687a9950ac09f
PhamVanHuy-EIUer <huy.phamvan.cit22@eiu.edu.vn>
2026-03-02 16:33:26 +0700

    update middleware

--- 3bro-fe/src/middleware.ts
+++ 3bro-fe/src/middleware.ts
@@ -1,40 +1,67 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-const protectedRoutes = [
-    { path: "/admin", roles: ["admin"] },
-    { path: "/user", roles: ["user", "admin"] },
-];
-
 export function middleware(req: NextRequest) {
-    const pathname = req.nextUrl.pathname;
+    const { pathname } = req.nextUrl;
 
     const refreshToken = req.cookies.get("refresh_token")?.value;
-
     const rolesCookie = req.cookies.get("role")?.value;
+
     const roleList = rolesCookie
         ? rolesCookie.split(",").map((r) => r.trim().toLowerCase())
         : [];
 
-    for (const route of protectedRoutes) {
-        if (pathname.startsWith(route.path)) {
-            if (!refreshToken) {
-                return NextResponse.redirect(new URL("/login", req.url));
-            }
-
-            const hasAccess = route.roles.some((r) =>
-                roleList.includes(r.toLowerCase())
-            );
-
-            if (!hasAccess) {
-                return NextResponse.redirect(new URL("/forbindden", req.url));
-            }
+    if (pathname === "/" && refreshToken) {
+        if (roleList.includes("admin")) {
+            return NextResponse.redirect(new URL("/admin", req.url));
         }
+
+        if (roleList.includes("user")) {
+            return NextResponse.redirect(new URL("/user", req.url));
+        }
+    }
+
+    if (pathname.startsWith("/admin")) {
+
+        // chưa login
+        if (!refreshToken) {
+            return NextResponse.redirect(new URL("/login", req.url));
+        }
+
+        if (roleList.length === 0) {
+            return NextResponse.redirect(new URL("/login", req.url));
+        }
+
+        // không phải admin
+        else if (!roleList.includes("admin")) {
+            return NextResponse.redirect(new URL("/forbindden", req.url));
+        }
+    }
+
+
+    if (pathname.startsWith("/user")) {
+
+        // chưa login
+        if (!refreshToken) {
+            return NextResponse.redirect(new URL("/login", req.url));
+        }
+        const hasAccess =
+            roleList.includes("user") || roleList.includes("admin");
+        if (roleList.length === 0) {
+            return NextResponse.redirect(new URL("/login", req.url));
+        }
+        // user hoặc admin đều được
+
+
+        else if (!hasAccess) {
+            return NextResponse.redirect(new URL("/forbindden", req.url));
+        }
+
     }
 
     return NextResponse.next();
 }
 
 export const config = {
-    matcher: ["/admin/:path*", "/user/:path*"],
+    matcher: ["/", "/admin/:path*", "/user/:path*"],
 };
